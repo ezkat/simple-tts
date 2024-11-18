bce1f3139ad9694e55d5fe7d2809ccf7aee737a7
ezkat <50319957+ezkat@users.noreply.github.com>
2024-11-19 00:43:39 +0200

    Set up CRUD API endpoints with permission checks

--- conversion/views.py
+++ conversion/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.db import transaction
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from .models import ConversionRequest
+from .serializers import ConversionRequestSerializer
 
-# Create your views here.
+
+
+class ConversionRequestViewSet(viewsets.ModelViewSet):
+    serializer_class = ConversionRequestSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = ConversionRequest.objects.filter(user=self.request.user)
+        return queryset
+    
+    @transaction.atomic
+    def perform_create(self, serializer):
+        instance = serializer.save(user=self.request.user, status=ConversionRequest.PENDING)
+        return instance
+    
+    @transaction.atomic
+    def perform_update(self, serializer):
+        instance = serializer.save(user=self.request.user, status=ConversionRequest.PENDING)
+        return instance
--- simple_tts/urls.py
+++ simple_tts/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.conf import settings
 from django.views.static import serve
+from django.urls import path, include, re_path
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from django.urls import path, re_path
+from conversion.views import ConversionRequestViewSet
+
+router = DefaultRouter()
+router.register(r'conversion_requests', ConversionRequestViewSet, basename='conversion-request')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
     path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

e7acf7ffda17e6187b44074d69b046e8ee5e8d12
ezkat <50319957+ezkat@users.noreply.github.com>
2024-11-19 00:18:41 +0200

    Add token authentication (#2)
    * Add token authentication using djangorestframework-simplejwt

--- simple_tts/urls.py
+++ simple_tts/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.conf import settings
 from django.views.static import serve
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 
